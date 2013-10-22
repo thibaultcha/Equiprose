bd401830d96b1e04dc756142cd24c7d3c5127ffb
thibaultCha <thibaultcha@me.com>
2013-10-22 14:22:57 +0200

    Stylesheets coverage. Passing tests: prepareOutputDir(), compileStylesheets()

--- test/building.js
+++ test/building.js
@@ -1,9 +1,12 @@
 var assert = require('assert')
 , path     = require('path')
+, fs       = require('fs')
+, fse      = require('fs-extra')
 , build    = require('../lib/building.js')
 
 describe('building.js', function () {
 	var config  = build.parseConfig('test/test-sites/valid-site')
+    , assetsDir = path.join(config.sitePath, '_assets')
 	, outputDir = path.join(config.sitePath, config.buildDir)
 
 	describe('#parseConfig()', function () {
@@ -18,11 +21,29 @@ describe('building.js', function () {
         })
     })
 
-	describe.skip('#prepareOutputDir()', function () {
-		it('should initialize an empty buildDir directory', function (done) {
-			build.prepareOutputDir(outputDir, function (err) {
+	describe('#prepareOutputDir()', function () {
+        it('should remove the buildDir if already present', function (done) {
+            before(function (done) {
+                if (fs.existsSync(outputDir)) {
+                    fse.remove(outputDir, function (err) {
+                        assert.ifError(err)
+                        done()
+                    })
+                }
+            })
+
+            build.prepareOutputDir(outputDir, assetsDir, function (err) {
+                assert.ifError(err)
+                assert(fs.existsSync(outputDir))
+                done()
+            })
+        })
+		it('should initialize a buildDir directory containing the required folders', function (done) {
+			build.prepareOutputDir(outputDir, assetsDir, function (err) {
 				assert.ifError(err)
-				assert(fs.existsSync(outputDir))
+                assert(fs.existsSync(path.join(outputDir, 'assets/js')), 'No assets/js folder')
+                assert(fs.existsSync(path.join(outputDir, 'assets/img')), 'No assets/img folder')
+                assert(fs.existsSync(path.join(outputDir, 'assets/files')), 'No assets/files folder')
 				done()
 			})
 		})
@@ -36,7 +57,7 @@ describe('building.js', function () {
 	})
 
 	describe('#fetchBlogPosts()', function () {
-        var posts 
+        var posts
 
         beforeEach(function () {
         	posts = build.fetchBlogPosts(config.sitePath)
@@ -57,11 +78,29 @@ describe('building.js', function () {
     })
 
     describe.skip('#compileStylesheets()', function () {
-        it('should compile all Stylus files from templates/styl to buildDir/assets/css', function (done) {
-            console.log(path.join(sitePath, stylDir))
+        
+
+        before(function (done) {
+            fse.remove(outputDir, function (err) {
+                assert.ifError(err)
+                done()
+            })
+        })
+
+        it('using fse#mkdirs() should create the buildDir if not existing', function (done) {
+            
+        })
+        it('should compile all Stylus files from templates/styl to buildDir/css', function (done) {
             lib.walk(path.join(sitePath, stylDir), new RegExp(/\.styl$/), function (err, results) {
                 assert.ifError(err)
                 console.log(results)
+                done()
+            })
+        })
+
+        after(function (done) {
+            fse.remove(outputDir, function (err) {
+                assert.ifError(err)
                 done()
             })
         })

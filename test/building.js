0600c697c73748014b7631282275b07d45b3835f
thibaultCha <thibaultcha@me.com>
2013-10-22 16:22:11 +0200

    Fix missing _assets directory in test-sites/valid-site. CompileStylesheets() passing some tests.

--- test/building.js
+++ test/building.js
@@ -3,11 +3,11 @@ var assert = require('assert')
 , fs       = require('fs')
 , fse      = require('fs-extra')
 , build    = require('../lib/building.js')
+, helpers  = require('../lib/helpers.js')
 
 describe('building.js', function () {
 	var config  = build.parseConfig('test/test-sites/valid-site')
     , assetsDir = path.join(config.sitePath, '_assets')
-	, outputDir = path.join(config.sitePath, config.buildDir)
 
 	describe('#parseConfig()', function () {
         it('should return an object', function () {
@@ -22,7 +22,11 @@ describe('building.js', function () {
     })
 
 	describe('#prepareOutputDir()', function () {
-        it('should remove the buildDir if already present', function (done) {
+        var outputDir  = path.join(config.sitePath, config.buildDir)
+        , assetsDir    = path.join(config.sitePath, '_assets')
+        , assetsOutput = path.join(outputDir, 'assets')
+
+        it('should recreate the buildDir if already present', function (done) {
             before(function (done) {
                 if (fs.existsSync(outputDir)) {
                     fse.remove(outputDir, function (err) {
@@ -32,18 +36,18 @@ describe('building.js', function () {
                 }
             })
 
-            build.prepareOutputDir(outputDir, assetsDir, function (err) {
+            build.prepareOutputDir(outputDir, assetsDir, assetsOutput, function (err) {
                 assert.ifError(err)
-                assert(fs.existsSync(outputDir))
+                assert(fs.existsSync(outputDir), 'buildDir has not been created')
                 done()
             })
         })
 		it('should initialize a buildDir directory containing the required folders', function (done) {
-			build.prepareOutputDir(outputDir, assetsDir, function (err) {
+			build.prepareOutputDir(outputDir, assetsDir, assetsOutput, function (err) {
 				assert.ifError(err)
-                assert(fs.existsSync(path.join(outputDir, 'assets/js')), 'No assets/js folder')
-                assert(fs.existsSync(path.join(outputDir, 'assets/img')), 'No assets/img folder')
-                assert(fs.existsSync(path.join(outputDir, 'assets/files')), 'No assets/files folder')
+                assert(fs.existsSync(path.join(assetsOutput, 'js')), 'No assets/js folder')
+                assert(fs.existsSync(path.join(assetsOutput, 'img')), 'No assets/img folder')
+                assert(fs.existsSync(path.join(assetsOutput, 'files')), 'No assets/files folder')
 				done()
 			})
 		})
@@ -78,28 +82,39 @@ describe('building.js', function () {
     })
 
     describe.skip('#compileStylesheets()', function () {
-        
+        var stylPath = path.join(config.sitePath, '_template/styl')
+        , outputCss  = path.join(config.sitePath, 'rendering-css')
 
-        before(function (done) {
-            fse.remove(outputDir, function (err) {
+        beforeEach(function (done) {
+            fse.remove(outputCss, function (err) {
                 assert.ifError(err)
                 done()
             })
         })
 
-        it('using fse#mkdirs() should create the buildDir if not existing', function (done) {
-            
-        })
-        it('should compile all Stylus files from templates/styl to buildDir/css', function (done) {
-            lib.walk(path.join(sitePath, stylDir), new RegExp(/\.styl$/), function (err, results) {
+        it('using fse#mkdirs() should create outputCss folder if not existing', function (done) {
+            build.compileStylesheets(stylPath, outputCss, function (err) {
                 assert.ifError(err)
-                console.log(results)
+                assert(fs.existsSync(outputCss), 'outputCss folder was not created')
                 done()
             })
         })
+        it('should compile all Stylus files from template to outputCss', function (done) {
+            build.compileStylesheets(stylPath, outputCss, function (err) {
+                assert.ifError(err)
+                helpers.getFiles(stylPath, new RegExp(/\.styl$/), function (err, items) {
+                    assert.ifError(err)
+                    items.forEach(function (item, idx) {
+                        var cssFile = item.replace(stylPath, outputCss).replace(/\.styl$/, '.css')
+                        assert(fs.existsSync(path.join(stylPath, cssFile)), 'Inexistant css file: ' + cssFile + ' for file: ' + item)
+                        if (idx == items.length - 1) done()
+                    })
+                })
+            })
+        })
 
-        after(function (done) {
-            fse.remove(outputDir, function (err) {
+        afterEach(function (done) {
+            fse.remove(outputCss, function (err) {
                 assert.ifError(err)
                 done()
             })
